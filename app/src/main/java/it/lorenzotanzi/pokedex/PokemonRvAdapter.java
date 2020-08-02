f7f20b43953c63ccd09e64a29d8fe6e6352bee6d
LorenzoT8942 <lorenzo.tanzi997@gmail.com>
2020-08-02 10:26:42 +0200

    Code cleaned.

--- app/src/main/java/it/lorenzotanzi/pokedex/PokemonRvAdapter.java
+++ app/src/main/java/it/lorenzotanzi/pokedex/PokemonRvAdapter.java
@@ -2,26 +2,17 @@ package it.lorenzotanzi.pokedex;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.LinearGradient;
-import android.graphics.Shader;
-import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.RectShape;
-import android.net.sip.SipSession;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.solver.widgets.Rectangle;
 import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
@@ -79,9 +70,9 @@ public class PokemonRvAdapter extends RecyclerView.Adapter<PokemonRvAdapter.View
 
         String idString = pokemonList.get(position).getPkmnNum().toString();
         String pkmnNameString = pokemonList.get(position).getPkmnName();
-        String type1str = pokemonList.get(position).getType1();
-        String type2str = pokemonList.get(position).getType2();
-        String type1col = colors.get(type1str);
+         String type1str = pokemonList.get(position).getType1();
+         String type2str = pokemonList.get(position).getType2();
+         String type1col = colors.get(type1str);
 
 
         //REQUEST .PNG OF THE POKEMON SPRITE TO SET
@@ -112,7 +103,7 @@ public class PokemonRvAdapter extends RecyclerView.Adapter<PokemonRvAdapter.View
             String type2col = colors.get(type2str);
             tv_pkmn_type2.setText(type2str);
             viewHolder.tv_pkmn_type2.setVisibility(View.VISIBLE);
-             int[] gradientColors = {Color.parseColor(type1col), Color.parseColor(type2col)};
+             int[] gradientColors = {Color.parseColor((type1col)), Color.parseColor(type2col)};
              Log.d("ADAPTER", "COLORS:" + type1str + type1col + ", " + type2str + type2col);
              GradientDrawable gd = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, gradientColors);
              gd.setGradientType(GradientDrawable.LINEAR_GRADIENT);
